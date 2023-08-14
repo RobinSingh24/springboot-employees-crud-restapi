fb77c23083d95e98063c0a492da166de14fe7296
Robin SIngh <robin.122497@gmail.com>
2023-08-15 04:15:22 +0530

    Added controller mappings

--- src/main/java/com/robinsingh/employees/rest/EmployeeRestController.java
+++ src/main/java/com/robinsingh/employees/rest/EmployeeRestController.java
@@ -4,9 +4,7 @@ import com.robinsingh.employees.dao.EmployeeDao;
 import com.robinsingh.employees.entity.Employee;
 import com.robinsingh.employees.service.EmployeeService;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,7 +19,44 @@ public class EmployeeRestController {
     }
 
     @GetMapping("/employees")
-    public List<Employee> findAll(){
+    public List<Employee> findAll() {
         return employeeService.findAll();
     }
+
+    @GetMapping("/employees/{employeeId}")
+    public Employee findById(@PathVariable int employeeId) {
+        Employee theEmployee = employeeService.findById(employeeId);
+
+        if (theEmployee == null)
+            throw new RuntimeException("Employee ID:" + employeeId + " not found");
+
+        return theEmployee;
+    }
+
+    @PostMapping("/employees")
+    public Employee createEmployee(@RequestBody Employee theEmployee) {
+
+        theEmployee.setId(0); // in case user passes an id in request body
+
+        return employeeService.save(theEmployee);
+    }
+    @PutMapping("/employees")
+    public Employee update(@RequestBody Employee theEmployee){
+        Employee dbEmployee = employeeService.findById(theEmployee.getId());
+        if(dbEmployee == null)
+            throw new RuntimeException("Employee Id:" + theEmployee.getId() + " is not an existing employee");
+
+        return employeeService.save(theEmployee);
+    }
+
+    @DeleteMapping("/employees/{employeeId}")
+    public String deleteEmployee(@PathVariable int employeeId){
+        Employee dbEmployee = employeeService.findById(employeeId);
+        if(dbEmployee == null)
+            throw new RuntimeException("No such employee with Id: " + employeeId);
+
+        employeeService.deleteById(employeeId);
+        return "Deleted employee ID: "+employeeId;
+    }
+
 }
